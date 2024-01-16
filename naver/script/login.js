77d4735f4a0c72f5611cf1a12af796be23618a58
won <won00yeom@naver.com>
2024-01-16 16:52:16 +0900

    ID 로그인 디자인

--- naver/script/login.js
+++ naver/script/login.js
@@ -15,12 +15,18 @@ console.log(id_login_container)
 console.log(disposable_login_container)
 console.log(qr_login_container)
 
+// 초기 일회용, QR 내용 숨기기
 disposable_login_container.style.display = 'none'
 qr_login_container.style.display = 'none'
+// 초기값 ID로그인 활성화 시키기(active) *클릭 전
+login_title[0].parentElement.classList.add('active')
 
 // ID 로그인 클릭하면 로그인 내용O, 일회용 내용X, QR코드 내용X
 login_title[0].addEventListener('click',()=>{
     console.log('.')
+    login_title[0].parentElement.classList.add('active')
+    login_title[1].parentElement.classList.remove('active')
+    login_title[2].parentElement.classList.remove('active')
     id_login_container.style.display = 'block'
     disposable_login_container.style.display = 'none'
     qr_login_container.style.display = 'none'
@@ -28,6 +34,9 @@ login_title[0].addEventListener('click',()=>{
 // 일회용 번호 클릭 시 로그인 내용X, 일회용 내용O, QR코드 내용X
 login_title[1].addEventListener('click',()=>{
     console.log('.')
+    login_title[0].parentElement.classList.remove('active')
+    login_title[1].parentElement.classList.add('active')
+    login_title[2].parentElement.classList.remove('active')
     id_login_container.style.display = 'none'
     disposable_login_container.style.display = 'block'
     qr_login_container.style.display = 'none'
@@ -35,6 +44,9 @@ login_title[1].addEventListener('click',()=>{
 // QR코드 클릭하면 로그인 내용X, 일회용 내용X, QR코드 내용O
 login_title[2].addEventListener('click',()=>{
     console.log('.')
+    login_title[0].parentElement.classList.remove('active')
+    login_title[1].parentElement.classList.remove('active')
+    login_title[2].parentElement.classList.add('active')
     id_login_container.style.display = 'none'
     disposable_login_container.style.display = 'none'
     qr_login_container.style.display = 'block'
